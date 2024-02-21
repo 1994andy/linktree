b9e26b988aac4151936db0c46c7fa249052eed7b
Annddy <annddystars3@gmail.com>
2024-02-21 19:06:36 -0300

    readme

---
+++ README.MD
@@ -0,0 +1,31 @@
+<h1 align="center"> LinkTree basic project </h1>
+
+<img src=".github/preview.jpg"> 
+
+### 🏠 [Homepage](https://github.com/1994andy/linktree)
+
+## Languages
+- CSS
+- Javascript
+- HTML
+
+
+
+
+
+## Author
+
+👤 **1994Andy**
+
+- Twitter: [@Annddyone](https://twitter.com/Annddyone)
+- Github: [@1994Andy](https://github.com/1994Andy)
+- LinkedIn: [@Anderson Souza](https://www.linkedin.com/in/anderson-souza-562903283/)
+
+
+
+
+
+## 📝 License
+
+Copyright © 2024 [Anderson Rosario] ( https://github.com/1994Andy ).<br />
+This project is [MIT] 
--- main.js
+++ main.js
@@ -1,16 +1,15 @@
 function toggleMode() {
     const html = document.documentElement
     html.classList.toggle('light')
-    //pegar a imagem
-const img = document.querySelector("#profile img")
+    const img = document.querySelector("#profile img")
 
     if (html.classList.contains('light')) {
         img.setAttribute('src', './Avatar-dark.png')
         img.setAttribute('alt', 'imagem-dark')
-        
-    }else{
-       img.setAttribute('src', './avatar.png')
-       img.setAttribute('alt', 'imagem-light')
+
+    } else {
+        img.setAttribute('src', './avatar.png')
+        img.setAttribute('alt', 'imagem-light')
     }
 
 }
